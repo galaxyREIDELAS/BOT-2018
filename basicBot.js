697b394a6220c566843770aefadbbd074250d141
GalaxyDELAS <30747073+GalaxyDELAS@users.noreply.github.com>
2017-09-25 18:06:19 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -261,7 +261,7 @@
             botName: '-gαℓαxy',
             language: 'portuguese',
             chatLink: 'https://rawgit.com/basicBot/source/master/lang/en.json',
-            scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js'',
+            scriptLink: 'https://rawgit.com/basicBot/source/master/basicBot.js',
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
